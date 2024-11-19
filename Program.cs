7e6847ee498b1f6fe588eedb3e263eab3c8d104b
F0sya <nikitachmyria@gmail.com>
2024-11-19 19:19:26 +0200

    First task completed

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Classes_Methods
+﻿using System.ComponentModel;
+
+namespace Classes_Methods
 {
     internal class Program
     {
+
+        static int RangeMultiply(int l, int r)
+        {
+            int res = 1;
+
+            for(int i = l; i <= r; i++)
+            {
+                res *= i;
+            }
+
+            return res;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x, y;
+            Console.Write("Enter left limit:"); 
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter right limit:"); 
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Result of operation: {RangeMultiply(x,y)}");
+
         }
+
     }
 }
