12c277f751a7ffafbce2b567b7a8afcb1f58c56e
F0sya <nikitachmyria@gmail.com>
2024-11-30 13:38:42 +0200

    Fourth practice task is completed

---
+++ Entities/City.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Methods.Entities
+{
+    internal class City
+    {
+        public string? CityName { get; set; }
+        public string? CountryName { get; set; }
+        public string? CountryCode { get; set; }
+        public int? Amount { get; set; }
+
+        public string[] Districts { get; set; }
+        public City() {
+            CityName = "Empty";
+            CountryName = "Empty";
+            CountryCode = "Empty";
+            Amount = 0;
+            Districts = ["Empty"];
+        }
+        public City(string cityName)
+        {
+            CityName = cityName;
+        }
+
+        public City(string cityName, string countryName) : this(cityName)
+        {
+            CityName = cityName;
+            CountryName = countryName;
+        }
+
+        public City(string cityName, string countryName, string countryCode) : this(cityName, countryName)
+        {
+            CityName = cityName;
+            CountryName = countryName;
+            CountryCode = countryCode;
+        }
+
+        public City(string cityName, string countryName, string countryCode, int amount) : this(cityName, countryName, countryCode)
+        {
+            CityName = cityName;
+            CountryName = countryName;
+            CountryCode = countryCode;
+            Amount = amount;
+        }
+
+        public City(string cityName, string countryName, string countryCode, int amount, string[] districts) : this(cityName, countryName, countryCode, amount)
+        {
+            CityName = cityName;
+            CountryName = countryName;
+            CountryCode = countryCode;
+            Amount = amount;
+            Districts = districts;
+        }
+
+        public override string ToString()
+        {
+            return $"City name: {CityName}\n" +
+                $"Country name: {CountryName}\n" +
+                $"Country number code: {CountryCode}\n" +
+                $"Amount of people: {Amount}\n" +
+                $"Districts: {String.Join(", ", Districts)}";
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,58 +1,22 @@
-﻿using System.ComponentModel;
+﻿using Classes_Methods.Entities;
+using System.ComponentModel;
 
 namespace Classes_Methods
 {
     internal class Program
     {
-        static void Bubble(int[] arr, int choice)
-        {
-            for(int n = arr.Length; n > 0; n--)
-            {
-                for (int i = 1; i < n; i++)
-                {
-                    if(choice == 1)
-                    {
-                        if (arr[i - 1] > arr[i])
-                        {
-                            (arr[i], arr[i - 1]) = (arr[i - 1], arr[i]);
-                        }
-                    }
-                    else if (choice == 2)
-                    {
-                        if (arr[i - 1] < arr[i])
-                        {
-                            (arr[i], arr[i - 1]) = (arr[i - 1], arr[i]);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error!");
-                        Environment.Exit(0);
-                    }
-                }
-            }
-        }
         static void Main(string[] args)
         {
-            int[] arr = [5,8,9,1,3,4];
-            int choice;
-            Console.Write("Before sorting: ");
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
-
-            Console.WriteLine("\n(1) Ascending order\n(2) Descending order");
-            choice = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("After sorting: ");
-            Bubble(arr, choice);
-            foreach (var item in arr)
-            {
-                Console.Write(item + " ");
-            }
 
 
+
+            City city = new City(cityName: "Vinnytsia",
+                countryName: "Ukraine",
+                countryCode: "+380", 
+                amount: 2000, 
+                districts: ["Vyshen'ka","Center","Slavyanka"]);
+           
+            Console.Write(city);
 
 
 
