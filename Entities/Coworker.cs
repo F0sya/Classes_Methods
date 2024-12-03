6824a2c2f3c430f6a2665026a5b8a96227a8c7ab
F0sya <nikitachmyria@gmail.com>
2024-12-03 19:12:21 +0200

    Fifth practice task is completed

---
+++ Entities/Coworker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Methods.Entities
+{
+    internal class Coworker
+    {
+        private string Name { get; set; }
+        private string BirthdayDate { get; set; }
+        private string PhoneNumber { get; set; }
+        private string Email { get; set; }
+        private string Position { get; set; }
+        private string[] JobChoirs { get; set; }
+    
+        
+        public Coworker() { }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter full name of coworker: ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Enter birthday date of coworker: ");
+            BirthdayDate = Console.ReadLine();
+
+            Console.WriteLine("Enter email of coworker: ");
+            Email = Console.ReadLine();
+
+            Console.WriteLine("Enter position of coworker: ");
+            Position = Console.ReadLine();
+
+            Console.WriteLine("Enter job choirs of coworker(divided by spaces): ");
+            JobChoirs = Console.ReadLine().Split();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"\nFull name: {Name}");
+            Console.WriteLine($"Birthday date: {BirthdayDate}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Position: {Position}");
+            Console.WriteLine("Choirs: ");
+            foreach(string choir in JobChoirs)
+            {
+                Console.Write(choir + " ");
+            }
+        }
+    
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,31 +1,16 @@
-﻿using System.ComponentModel;
+﻿using Classes_Methods.Entities;
+using System.ComponentModel;
 
 namespace Classes_Methods
 {
     internal class Program
     {
-        static bool IsSquare(int x)
-        {
-            int num = (int)Math.Sqrt(x);
-            return num * num == x;
-        }
-        static bool IsFibo(int num)
-        {
-            if(num < 0) return false;
-
-            return (IsSquare(5 * num * num + 4) || IsSquare(5 * num * num - 4)) ? true : false;
-        }
         static void Main(string[] args)
         {
-            int x, y;
-            Console.Write("Enter a number:"); 
-            x = Convert.ToInt32(Console.ReadLine());
+            Coworker coworker = new Coworker();
 
-            Console.WriteLine((IsFibo(x) == true) ? "This number is a fibonacci" : "This number isn't a fibonacci");
-
-
-
+            coworker.Input();
+            coworker.Print();
         }
-
     }
 }
