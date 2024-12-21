3ac2a90e46a31b8feb01b2e73b746e6d8fee0aa5
F0sya <nikitachmyria@gmail.com>
2024-12-21 13:45:18 +0200

    Task 1 is completed

--- Program.cs
+++ Program.cs
@@ -1,14 +1,36 @@
-﻿using Classes_Methods.Entities;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.Data;
 
 namespace Classes_Methods
 {
     internal class Program
     {
+        static void Sqr(char x)
+        {
+            for (int i = 0; i < 10; i++){
+                Console.Write(x + " ");
+            }
+            Console.Write('\n');
+            for(int i = 0; i < 8; i++)
+            {
+                Console.Write(x);
+                for(int k = 0; k < 17; k++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(x);
+                Console.Write('\n');
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(x + " ");
+            }
+        }
         static void Main(string[] args)
         {
-            Plane plane = new Plane("Antonov An-225 Mriya", "Antonov Serial Production Plant", 1985, "Transport");
-            plane.Print();
+            Console.WriteLine("Enter an char:");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            Sqr(symbol);
         }
     }
 }
