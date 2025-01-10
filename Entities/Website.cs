c5f43dc6d48226ca4b22cd2e201140462e4f59dc
F0sya <nikitachmyria@gmail.com>
2025-01-11 01:07:43 +0200

    Fourth homework exercise is done

---
+++ Entities/Website.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Methods.Entities
+{
+    internal class Website
+    {
+        private string Url { get; set; }
+        private string Name { get; set; }
+        private string Description { get; set; }
+        private string Ip { get; set; }
+
+        public Website()
+        {
+            Url = "NaN";
+            Name = "NaN";
+            Description = "NaN";
+            Ip = "0.0.0.0";
+        }
+        public Website(string url, string name, string description, string ip)
+        {
+            Url = url;
+            Name = name;
+            Description = description;
+            Ip = ip;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter a name of website: ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Enter an url: ");
+            Url = Console.ReadLine();
+
+            Console.WriteLine("Enter a description:");
+            Description = Console.ReadLine();
+
+            Console.WriteLine("Enter an ip address of website:");
+            Ip = Console.ReadLine();
+        }
+
+        public void Input(string url, string name, string description, string ip)
+        {
+            this.Name = name;
+            this.Url = url;
+            this.Description = description;
+            this.Ip= ip;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"\nWebsite info:\n" +
+                $"Name: {Name}\n" +
+                $"Url: {Url}\n" +
+                $"Description: {Description}\n" +
+                $"Ip: {Ip}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,47 +2,16 @@
 using System.Data;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using Classes_Methods.Entities;
 
 namespace Classes_Methods
 {
     internal class Program
     {
-        static int[] Filtering(int[] original, int[] filter)
-        {
-            List<int> result = new List<int>();
-
-            for(int i =0; i < original.Length; i++)
-            {
-                if (!Array.Exists(filter,element => element == original[i]))
-                {
-                    result.Add(original[i]);
-                }
-            }
-
-            return result.ToArray();
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a size of a list:");
-            int size = Convert.ToInt32(Console.ReadLine());
-
-            int[] arr = new int[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                Console.WriteLine($"Enter an {i + 1} number:");
-                arr[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            int[] filter_arr = [1,2,3,4,5];
-
-            int[] res = Filtering(arr, filter_arr);
-
-            Console.WriteLine("Result:");
-            for(int i = 0; i < res.Length; i++)
-            {
-                Console.Write($"{res[i]} ");
-            }
+            Website website = new Website("mystat.itstep.org", "Mystat ITSTEP", "ITSTEP diary", "255.255.255.255");
+            website.Print();
         }
     }
 }
